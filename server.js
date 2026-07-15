3e33323f936d609cb82a23feece41fbb220db845
ShizaAhsan <shizaahsan2006@gmail.com>
2026-07-15 23:49:08 +0500

    Stage 2: read endpoints with 404

--- server.js
+++ server.js
@@ -2,6 +2,44 @@ const express=require("express")
 const app=express();
 const PORT=3000;
 
+const tasks=[
+    {
+        id : 1,
+        title:"Learn Express",
+        done: false
+    },
+    {
+        id: 2,
+        title:"Build Task API",
+        done:true
+
+    },
+    {
+        id:3,
+        title:"Test with curl",
+        done:false
+    }
+]
+
+app.get("/tasks",(req,res)=>
+{
+    res.json(tasks)
+})
+
+app.get("/tasks/:id",(req,res)=>
+{
+    const id=Number(req.params.id);
+
+    const foundTask =tasks.find(t=>t.id==id);
+    if(!foundTask)
+    {
+        return res.status(404).json({
+            error:`Task ${id} not found`
+        });
+    }
+
+    res.json(foundTask );
+})
 app.get("/",(req,res)=>
 {
     res.json(
